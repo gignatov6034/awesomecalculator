9e42dcdbe74bb55c1575e6ba075bf84574de3973
Gleb Ignatov <Gignatov6034@conestogac.on.ca>
2023-11-10 16:48:14 -0500

    Action

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="gignatov-awesomecalculator", # Replace with your own username
     version= "1.0.0",
-    author="Daniel Edreira",
-    author_email="dedreira@github.com",
+    author="Gleb Ignatov",
+    author_email="gignatov6034@github.com",
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/gignatov6034/awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
